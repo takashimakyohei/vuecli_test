534992b65629b8f0c3a93cd7f7391a7e4744b7d5
kyohei_takashima <kyohei.takashima@person-link.co.jp>
2020-03-06 19:08:09 +0900

    vuecliテスト

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <myheader></myheader>
+    <p v-if="message.length > 0">
+      {{message}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="message">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+import myheader from '/Users/takashimakyouhei/workspace/sample/src/components/myheader'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      message: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.message = ''
+    }
+  }
+}
+</script>
